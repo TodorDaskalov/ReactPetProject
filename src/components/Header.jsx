d90eec643321ad02e49d7ee266e0a7d7879701b5
Todor Daskalov <todorvdaskalov@gmail.com>
2023-11-12 18:57:21 +0200

    Crate new Header element and css for it

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,18 @@
+import { Link } from 'react-router-dom';
+import styles from './Header.module.css'
+
 export default function Header() {
     return (
-        <header>
-            <div className="container_12">
-                <div className="grid_12">
-                    <h1>Viki's Pet Nursery</h1>
-                    <div className="menu_block">
-                        <nav>
-                            <ul className="sf-menu">
-                                <li className="current">
-                                    <a href="index.html">Home</a>
-                                </li>
-                                <li className="with_ul">
-                                    <a href="about-us.html">About Us</a>
-                                </li>
-                                <li>
-                                    <a href="services.html">Services</a>
-                                </li>
-                                <li>
-                                    <a href="blog.html">Blog</a>
-                                </li>
-                                <li>
-                                    <a href="contacts.html">Contacts </a>
-                                </li>
-                            </ul>
-                        </nav>
-                        <div className="clear" />
-                    </div>
-                    <div className="clear" />
-                </div>
-            </div>
-        </header>
-    );
-};
+        <div className={styles.headerContainer}>
+            <h1 className={styles.header}>Viki's Pet Nursery</h1>
+            <nav className={styles.navElement}>
+                <ul className={styles.ulElement}>
+                    <Link to={'/'} className={styles.liElement}>Home page</Link>
+                    <Link to={'/pets'} className={styles.liElement}>Pet's for adoption</Link>
+                    <Link to={'/details'} className={styles.liElement}>Register / Login</Link>
+                    <Link to={'/tips'} className={styles.liElement}>Contacts</Link>
+                </ul>
+            </nav>
+        </div>
+    )
+}
