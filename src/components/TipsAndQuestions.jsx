2852bdba921c54214bc457048f7d8245f65679ea
Todor Daskalov <todorvdaskalov@gmail.com>
2023-11-12 18:58:04 +0200

    Create TipsAndQuestions element and css for it

---
+++ src/components/TipsAndQuestions.jsx
@@ -0,0 +1,44 @@
+export default function tipsAndQuestions() {
+    return (
+        <div className="bottom_block">
+            <div className="container_12">
+                <div className="grid_6">
+                    <h2>Pet Care Tips </h2>
+                    <br />
+                    Praesent quis orci eget diam viverra consequat. Fusce
+                    sagittis quam in pulvinar sollicitudin velit velit cursus
+                    nibh ullamcorper accumsan sem lectus ut sapien. Donec
+                    venenatis posuere velit a convallis neque ullamcorper quis.
+                    Integer posuere ipsum eu risus sollicitudin nec varius erat
+                    luctus. Fusce fringilla erat ac urna pe llentesque congue.
+                    Nunc fringilla, diam sit amet adipi scing bibendum turpis
+                    velit feugiat urna, et pharetra neque nisi ac nunc. Vivamus
+                    est quam dapibus ullamco rper imperdiet nec, euismod ut
+                    arcu. Nulla facilisi. Etiam mauris lorem pulvinar vel con
+                    sequat ut pretium ac erat. Morbi facilisis elit eu nisl
+                    blandit ac blandit enim faucibus. Praesent quis orci eget
+                    diam viverra consequat. Fusce sagittis.{" "}
+                </div>
+                <div className="grid_4 prefix_2">
+                    <h2 className="ic1">Any Question?</h2>
+                    <img
+                        src="images/page1_img4.jpg"
+                        alt=""
+                        className="fleft img_inner"
+                    />
+                    <div className="extra_wrapper">
+                        <div className="cont">
+                            {" "}
+                            Call Us Free: <span>+1 800 559 6580</span>{" "}
+                        </div>
+                    </div>
+                    <div className="clear" />
+                    Nunc fringilla, diam sit amet adipi scing bibendum turpis
+                    velit feugiat urna, et pharetra neque nisi ac nunc. Viv amus
+                    est quam dapibus ullamco rper imperdiet nec euismod ut arcu.
+                    Nulla facilisi. Etiam mauris.{" "}
+                </div>
+            </div>
+        </div>
+    );
+}
