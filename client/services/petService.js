1f5ad00b2233456334052f9afa7d09404390b4df
Todor Daskalov <todorvdaskalov@gmail.com>
2023-11-17 21:55:39 +0200

    Add ; in the end of the row

--- client/services/petService.js
+++ client/services/petService.js
@@ -1,6 +1,5 @@
 export const getPetDetails = async (petId) => {
-     const result = await fetch(`http://localhost:3030/jsonstore/pets/${petId}`)
-     
+     const result = await fetch(`http://localhost:3030/jsonstore/pets/${petId}`);
      const data = await result.json();
      
      return data;
