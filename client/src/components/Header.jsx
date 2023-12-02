e03bfe77089db61ced870b989369da1cbb47c913
Todor Daskalov <todorvdaskalov@gmail.com>
2023-12-02 16:26:20 +0200

    Add paths and verification if user is authenticated

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,33 @@
 import { Link } from 'react-router-dom';
 import styles from './Header.module.css'
+import { useContext } from 'react';
+import AuthContext from '../contexts/AuthContext';
 
 export default function Header() {
+
+    const {
+        isAuthenticated,
+        email,
+        username
+    } = useContext(AuthContext);
+
     return (
         <div className={styles.headerContainer}>
             <h1 className={styles.header}>Viki's Pet Nursery</h1>
             <nav className={styles.navElement}>
                 <ul className={styles.ulElement}>
                     <Link to={'/'} className={styles.liElement}>Home page</Link>
-                    <Link to={'/add-pet'} className={styles.liElement}>Add pet</Link>
                     <Link to={'/pets'} className={styles.liElement}>Pet's for adoption</Link>
-                    <Link to={'/login'} className={styles.liElement}>Register / Login</Link>
+                    {isAuthenticated && ( 
+                        <>
+                            <Link to={'/add-pet'} className={styles.liElement}>Add pet</Link>
+                            <Link to={'/logout'} className={styles.liElement}>Logout</Link>
+                            <span>Hello {username} </span>
+                        </>
+                    )}
+                    {!isAuthenticated && (
+                        <Link to={'/login'} className={styles.liElement}>Register / Login</Link>
+                    )}
                     <Link to={'/tips'} className={styles.liElement}>Contacts</Link>
                 </ul>
             </nav>
