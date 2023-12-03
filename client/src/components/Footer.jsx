66d5ae994565dcdb6e9456447f2514eedde11d46
Todor Daskalov <todorvdaskalov@gmail.com>
2023-12-03 13:18:46 +0200

    Removed social links

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,12 +4,6 @@ const Footer = () => {
   return (
     <footer className={styles.footerContainer}>
       <div className={styles.gridContainer}>
-        <div className={styles.socials}>
-          <a href="#">icon</a>
-          <a href="#">icon</a>
-          <a href="#">icon</a>
-          <a href="#">icon</a>
-        </div>
         <p>Viki's Pet Nursery ©</p>
       </div>
     </footer>
