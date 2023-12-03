579c7d12015cb2df4b052bad8879e37af7f069e6
Todor Daskalov <todorvdaskalov@gmail.com>
2023-12-04 00:41:16 +0200

    Create Comment component with css for it

---
+++ client/src/components/Comment.jsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import styles from "./Comment.module.css";
+
+export default function Comment({ comment, onEdit, onDelete }) {
+    const [isEditing, setIsEditing] = useState(false);
+    const [editedContent, setEditedContent] = useState(comment.content);
+
+    const handleEditClick = () => {
+        setIsEditing(true);
+    };
+
+    const handleSaveClick = () => {
+        onEdit(comment._id, editedContent);
+        setIsEditing(false);
+    };
+
+    const handleCancelClick = () => {
+        setIsEditing(false);
+        setEditedContent(comment.content);
+    };
+
+    const handleDeleteClick = () => {
+        onDelete(comment._id);
+    };
+
+    const handleContentChange = (e) => {
+        setEditedContent(e.target.value);
+    };
+
+    return (
+        <li key={comment._id} className={styles.commentItem}>
+            <div>{comment._ownerId}</div>
+            <div className={styles.commentBox}>
+                {isEditing ? (
+                    <>
+                        <textarea
+                            value={editedContent}
+                            onChange={handleContentChange}
+                            className={`${styles.commentInput} ${isEditing ? styles.editing : ''}`}
+                            
+                        />
+                        <div className={styles.commentButtons}>
+                            <button
+                                className={styles.saveButton}
+                                onClick={handleSaveClick}
+                            >
+                                Save
+                            </button>
+                            <button
+                                className={styles.cancelButton}
+                                onClick={handleCancelClick}
+                            >
+                                Cancel
+                            </button>
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <p>{comment.content}</p>
+                        <div className={styles.commentButtons}>
+                            <button
+                                className={styles.editButton}
+                                onClick={handleEditClick}
+                            >
+                                Edit
+                            </button>
+                            <button
+                                className={styles.deleteButton}
+                                onClick={handleDeleteClick}
+                            >
+                                Delete
+                            </button>
+                        </div>
+                    </>
+                )}
+            </div>
+        </li>
+    );
+}
