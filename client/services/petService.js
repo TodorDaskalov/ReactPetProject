d9d6f9b984f42c058543129536e76037e6d4213d
Todor Daskalov <todorvdaskalov@gmail.com>
2023-12-04 00:39:58 +0200

    Import getHeaders function

--- client/services/petService.js
+++ client/services/petService.js
@@ -1,21 +1,25 @@
-export const getAllPets = async () => {
-    const baseUrl = "http://localhost:3030/jsonstore/pets";
+import { getHeaders } from "./authService";
 
-    const result = await fetch(baseUrl);
+const baseUrl = "http://localhost:3030/data";
+
+export const getAllPets = async () => {
+
+    const result = await fetch(`${baseUrl}/pets`);
     const data = await result.json();
 
-    return Object.values(data);
+    return data;
 };
 
 export const getPetDetails = async (petId) => {
-    const result = await fetch(`http://localhost:3030/jsonstore/pets/${petId}`);
+    const result = await fetch(`${baseUrl}/pets/${petId}`);
     const data = await result.json();
 
     return data;
 };
 
 export const deletePet = async (petId) => {
-    await fetch(`http://localhost:3030/jsonstore/pets/${petId}`, {
+    await fetch(`${baseUrl}/pets/${petId}`, {
         method: "DELETE",
+        headers: getHeaders(),
     });
 };
