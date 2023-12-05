8e9e66ad1c0941e08e4d711b0e7016d9a67329bb
Todor Daskalov <todorvdaskalov@gmail.com>
2023-12-06 01:08:20 +0200

    Add edit pet functionality with updatePet function

--- client/services/petService.js
+++ client/services/petService.js
@@ -3,7 +3,6 @@ import { getHeaders } from "./authService";
 const baseUrl = "http://localhost:3030/data";
 
 export const getAllPets = async () => {
-
     const result = await fetch(`${baseUrl}/pets`);
     const data = await result.json();
 
@@ -22,4 +21,26 @@ export const deletePet = async (petId) => {
         method: "DELETE",
         headers: getHeaders(),
     });
+};
+
+export const updatePet = async (petId, petForm) => {
+    try {
+        const response = await fetch(
+            `http://localhost:3030/data/pets/${petId}`,
+            {
+                method: "PUT",
+                headers: getHeaders(),
+                body: JSON.stringify(petForm),
+            }
+        );
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        return response;
+    } catch (error) {
+        console.error("Error updating pet:", error);
+        throw error;
+    }
 };
