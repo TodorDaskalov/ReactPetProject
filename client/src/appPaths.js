12b5f47ae2d06b338eb6f4fe35b797275812deca
Todor Daskalov <todorvdaskalov@gmail.com>
2023-12-06 01:09:14 +0200

    Add edit-pet path

--- client/src/appPaths.js
+++ client/src/appPaths.js
@@ -1,12 +1,13 @@
 const Path = {
     Home: '/',
     AddPet: '/add-pet',
+    EditPet: 'pets/edit-pet/:petId',
     PetsList: '/pets',
     PetDetails: '/pets/:petId',
     Tips: '/tips',
     Login: '/login',
     Logout: '/logout',
-    Register: '/register'
+    Register: '/register',
 }
 
 export default Path
