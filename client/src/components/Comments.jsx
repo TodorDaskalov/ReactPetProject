e894597d8fc0cbd9c440c5db6ac7faad1f5cfeb4
Todor Daskalov <todorvdaskalov@gmail.com>
2023-12-06 01:10:00 +0200

    Add conditional rendering if the user is logged

--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import {
     getAllComments,
     postComment,
@@ -6,18 +6,23 @@ import {
     updateComment,
 } from "../../services/commentsService";
 import styles from "./Comments.module.css";
-
+import AuthContext from "../contexts/AuthContext";
 import Comment from "./Comment";
 
-export default function Comments({ petId, email }) {
+export default function Comments({ pet, petId, email }) {
     const [comment, setComment] = useState("");
     const [comments, setComments] = useState([]);
+    const { userId } = useContext(AuthContext);
+    const isOwner = pet._ownerId === userId;
 
     const addCommentHandler = async () => {
         if (comment.trim() !== "") {
             try {
                 const newComment = await postComment(petId, comment);
-                setComments(comments => [...comments, {...newComment, owner: {email: email}}]);
+                setComments((comments) => [
+                    ...comments,
+                    { ...newComment, owner: { email: email } },
+                ]);
                 setComment("");
             } catch (error) {
                 console.error("Error posting comment:", error);
@@ -39,7 +44,7 @@ export default function Comments({ petId, email }) {
     const editCommentHandler = async (commentId, newContent) => {
         try {
             await updateComment(commentId, newContent);
-            
+
             setComments((prevComments) =>
                 prevComments.map((comment) =>
                     comment._id === commentId
@@ -61,31 +66,37 @@ export default function Comments({ petId, email }) {
     return (
         <div className={styles.commentsSection}>
             <h2>Comments/Questions:</h2>
-            <ul className={styles.commentsList}>
-                {comments.map((comment) => (
-                    <Comment
-                        key={comment._id}
-                        comment={comment}
-                        onDelete={deleteCommentHandler}
-                        onEdit={editCommentHandler}
+            {comments.length > 0 ? (
+                <ul className={styles.commentsList}>
+                    {comments.map((comment) => (
+                        <Comment
+                            key={comment._id}
+                            comment={comment}
+                            onDelete={deleteCommentHandler}
+                            onEdit={editCommentHandler}
+                        />
+                    ))}
+                </ul>
+            ) : (
+                <p>No comments or questions at the moment.</p>
+            )}
+            {isOwner && (
+                <div className={styles.commentInputContainer}>
+                    <input
+                        type="text"
+                        value={comment}
+                        onChange={(e) => setComment(e.target.value)}
+                        placeholder="Add a comment/question..."
+                        className={styles.commentInput}
                     />
-                ))}
-            </ul>
-            <div className={styles.commentInputContainer}>
-                <input
-                    type="text"
-                    value={comment}
-                    onChange={(e) => setComment(e.target.value)}
-                    placeholder="Add a comment/question..."
-                    className={styles.commentInput}
-                />
-                <button
-                    onClick={addCommentHandler}
-                    className={styles.addCommentButton}
-                >
-                    Add Comment / Question
-                </button>
-            </div>
+                    <button
+                        onClick={addCommentHandler}
+                        className={styles.addCommentButton}
+                    >
+                        Add Comment / Question
+                    </button>
+                </div>
+            )}
         </div>
     );
 }
