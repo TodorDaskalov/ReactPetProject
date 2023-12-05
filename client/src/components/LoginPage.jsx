eee70bd8366b93b6a6c6d6f22a50d2474cc7db4f
Todor Daskalov <todorvdaskalov@gmail.com>
2023-12-06 01:10:18 +0200

    Add data validation

--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -3,45 +3,73 @@ import styles from "./LoginPage.module.css";
 import AuthContext from "../contexts/AuthContext";
 
 const LoginPage = () => {
-  const {loginSubmitHandler} = useContext(AuthContext);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+    const { loginSubmitHandler } = useContext(AuthContext);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
 
-  const handleLogin = (e) => {
-    e.preventDefault();
-    loginSubmitHandler(email, password)
-  };
+    const handleLogin = async (e) => {
+        e.preventDefault();
 
-  return (
-    <div className={styles.loginContainer}>
-      <h2 className={styles.loginTitle}>Login</h2>
-      <form className={styles.loginForm} onSubmit={handleLogin}>
-        <label className={styles.label}>
-          Email:
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className={styles.input}
-            autoComplete="email"
-          />
-        </label>
-        <label className={styles.label}>
-          Password:
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className={styles.input}
-            autoComplete="current-password"
-          />
-        </label>
-        <button type="submit" className={styles.loginButton}>
-          Login
-        </button>
-      </form>
-    </div>
-  );
+        if (!email || !isValidEmail(email)) {
+            setError("Please enter a valid email address.");
+            return;
+        }
+
+        if (!password || password.length < 6) {
+            setError("Password must be at least 6 characters.");
+            return;
+        }
+
+        try {
+            setError("");
+
+            await loginSubmitHandler(email, password);
+        } catch (error) {
+            if (error.response && error.response.data) {
+                setError(error.response.data.message);
+            } else {
+                setError("Incorrect email or password. Please try again.");
+            }
+        }
+    };
+
+    const isValidEmail = (value) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(value);
+    };
+
+    return (
+        <div className={styles.loginContainer}>
+            <h2 className={styles.loginTitle}>Login</h2>
+            {error && <p className={styles.error}>{error}</p>}
+            <form className={styles.loginForm} onSubmit={handleLogin}>
+                <label className={styles.label}>
+                    Email:
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        className={styles.input}
+                        autoComplete="email"
+                    />
+                </label>
+                <label className={styles.label}>
+                    Password:
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        className={styles.input}
+                        autoComplete="current-password"
+                    />
+                </label>
+                <button type="submit" className={styles.loginButton}>
+                    Login
+                </button>
+            </form>
+        </div>
+    );
 };
 
 export default LoginPage;
