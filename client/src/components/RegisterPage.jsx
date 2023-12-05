f13ebcb44401c9234ccbac4ff81e340974bcadf6
Todor Daskalov <todorvdaskalov@gmail.com>
2023-12-06 01:12:39 +0200

    Add data validation

--- client/src/components/RegisterPage.jsx
+++ client/src/components/RegisterPage.jsx
@@ -3,19 +3,44 @@ import styles from "./RegisterPage.module.css";
 import AuthContext from "../contexts/AuthContext";
 
 const RegisterPage = () => {
-    const {registerSubmitHandler} = useContext(AuthContext);
+    const { registerSubmitHandler } = useContext(AuthContext);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
+    const [error, setError] = useState("");
 
     const handleRegister = (e) => {
         e.preventDefault();
+
+        if (!email || !isValidEmail(email)) {
+            setError("Please enter a valid email address.");
+            return;
+        }
+
+        if (!password || password.length < 6) {
+            setError("Password must be at least 6 characters.");
+            return;
+        }
+
+        if (password !== confirmPassword) {
+            setError("Passwords do not match.");
+            return;
+        }
+
+        setError("");
+
         registerSubmitHandler(email, password);
+    };
+
+    const isValidEmail = (value) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(value);
     };
 
     return (
         <div className={styles.registerContainer}>
             <h2 className={styles.registerTitle}>Register</h2>
+            {error && <p className={styles.error}>{error}</p>}
             <form className={styles.registerForm} onSubmit={handleRegister}>
                 <label className={styles.label}>
                     Email:
