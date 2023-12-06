70f98ea592581574ce69c509e5084191ca0045a1
Todor Daskalov <todorvdaskalov@gmail.com>
2023-12-07 01:02:22 +0200

    Create notFoundPage component with css

---
+++ client/src/components/notFoundPage.jsx
@@ -0,0 +1,12 @@
+import styles from "./notFoundPage.module.css";
+
+const NotFoundPage = () => {
+  return (
+    <div className={styles.notFoundContainer}>
+      <h1 className={styles.title}>404 - Not Found</h1>
+      <p className={styles.message}>Sorry, the page you are looking for might be in another universe.</p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
