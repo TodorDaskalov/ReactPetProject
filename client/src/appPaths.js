33143e2252cd61a3b76d8035639c533f2f334399
Todor Daskalov <todorvdaskalov@gmail.com>
2023-12-06 02:21:03 +0200

    Add my-pets path

--- client/src/appPaths.js
+++ client/src/appPaths.js
@@ -1,8 +1,9 @@
 const Path = {
     Home: '/',
     AddPet: '/add-pet',
-    EditPet: 'pets/edit-pet/:petId',
+    EditPet: '/pets/edit-pet/:petId',
     PetsList: '/pets',
+    MyPets: '/my-pets',
     PetDetails: '/pets/:petId',
     Tips: '/tips',
     Login: '/login',
