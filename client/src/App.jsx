7f95865e20eb0fcc31fb5a5fb234dfe666dee0f8
Todor Daskalov <todorvdaskalov@gmail.com>
2023-12-07 01:05:07 +0200

    Add ErrorBoundary and NotFoundPage component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import { AuthProvider } from "./contexts/AuthContext";
 
-import Path from "./appPaths"
+import Path from "./appPaths";
 
 import Footer from "./components/Footer";
 import Header from "./components/Header";
@@ -16,28 +16,41 @@ import LoginPage from "./components/LoginPage";
 import RegisterPage from "./components/RegisterPage";
 import Logout from "./components/Logout";
 import EditPet from "./components/EditPet";
+import ErrorBoundary from "./components/ErrorBoundary";
+import NotFoundPage from "./components/notFoundPage";
 
 function App() {
-    
     return (
-        <AuthProvider>
-            <>
-                <Header />
-                <Routes>
-                    <Route path={Path.Home} element={<HeroPage />} />
-                    <Route path={Path.AddPet} element={<CreatePet />} />
-                    <Route path={Path.EditPet} element={<EditPet />} />
-                    <Route path={Path.PetsList} element={<PetsList />} />
-                    <Route path={Path.MyPets} element={<MyPets />} />
-                    <Route path={Path.PetDetails} element={<PetDetails />} />
-                    <Route path={Path.Tips} element={<TipsAndQuestions />} />
-                    <Route path={Path.Login} element={<LoginPage />} />
-                    <Route path={Path.Logout} element={<Logout />} />
-                    <Route path={Path.Register} element={<RegisterPage />} />
-                </Routes>
-                <Footer />
-            </>
-        </AuthProvider>
+        <ErrorBoundary>
+            <AuthProvider>
+                <>
+                    <Header />
+                    <Routes>
+                        <Route path={Path.Home} element={<HeroPage />} />
+                        <Route path={Path.AddPet} element={<CreatePet />} />
+                        <Route path={Path.EditPet} element={<EditPet />} />
+                        <Route path={Path.PetsList} element={<PetsList />} />
+                        <Route path={Path.MyPets} element={<MyPets />} />
+                        <Route
+                            path={Path.PetDetails}
+                            element={<PetDetails />}
+                        />
+                        <Route
+                            path={Path.Tips}
+                            element={<TipsAndQuestions />}
+                        />
+                        <Route path={Path.Login} element={<LoginPage />} />
+                        <Route path={Path.Logout} element={<Logout />} />
+                        <Route
+                            path={Path.Register}
+                            element={<RegisterPage />}
+                        />
+                        <Route path="*" element={<NotFoundPage />} />
+                    </Routes>
+                    <Footer />
+                </>
+            </AuthProvider>
+        </ErrorBoundary>
     );
 }
 
