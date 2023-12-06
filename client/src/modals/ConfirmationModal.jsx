f678783f313374f4c29dc98692ceb4dfdff7f6e1
Todor Daskalov <todorvdaskalov@gmail.com>
2023-12-07 01:03:53 +0200

    Crate confirmation modal for deleting pet

---
+++ client/src/modals/ConfirmationModal.jsx
@@ -0,0 +1,23 @@
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+
+const ConfirmationModal = ({ show, handleClose, handleConfirm }) => {
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Confirm Deletion</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>Are you sure you want to delete this pet?</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Cancel
+        </Button>
+        <Button variant="danger" onClick={handleConfirm}>
+          Delete
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default ConfirmationModal;
